3d57f32b02f6967e435784e5c8884c78d6e2f70a
Adam Driscoll <adam.r.driscoll@gmail.com>
2017-03-07 16:58:19 -0600

    Updating NuGet package.

--- src/publish-packages.ps1
+++ src/publish-packages.ps1
@@ -1,4 +1,4 @@
-.\nuget.exe push CodeConverter.Common\bin\Release\codeconverter.common.1.0.0-alpha.nupkg -Source https://www.nuget.org/api/v2/package
-.\nuget.exe push CodeConverter.CSharp\bin\Release\codeconverter.csharp.1.0.0-alpha.nupkg -Source https://www.nuget.org/api/v2/package
-.\nuget.exe push CodeConverter.PowerShell\bin\Release\codeconverter.powershell.1.0.0-alpha.nupkg -Source https://www.nuget.org/api/v2/package
-.\nuget.exe push CodeConverter\bin\Release\codeconverter.1.0.0-alpha.nupkg -Source https://www.nuget.org/api/v2/package
+.\nuget.exe push CodeConverter.Common\bin\Release\codeconverter.common.1.0.1-alpha.nupkg -Source https://www.nuget.org/api/v2/package
+.\nuget.exe push CodeConverter.CSharp\bin\Release\codeconverter.csharp.1.0.1-alpha.nupkg -Source https://www.nuget.org/api/v2/package
+.\nuget.exe push CodeConverter.PowerShell\bin\Release\codeconverter.powershell.1.0.1-alpha.nupkg -Source https://www.nuget.org/api/v2/package
+.\nuget.exe push CodeConverter\bin\Release\codeconverter.1.0.1-alpha.nupkg -Source https://www.nuget.org/api/v2/package
